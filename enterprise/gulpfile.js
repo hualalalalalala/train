6422760c57b552adf54a6c7b9bb923dabad93fdf
hualalalalalala <490291490@qq.com>
2020-06-28 10:50:05 +0800

    企业站图片处理

--- enterprise/gulpfile.js
+++ enterprise/gulpfile.js
@@ -2,10 +2,10 @@ var gulp = require('gulp')
 var responsive = require('gulp-responsive')
 gulp.task('default', function () {
  return gulp
- .src('./images/*.{jpg,png}')
+ .src('./images/news/*.{jpg,png}')
  .pipe(
  responsive({
- '*.jpg': [
+ '*': [
  {
  width: 90,
  rename: {
@@ -22,39 +22,55 @@ gulp.task('default', function () {
  width: 360,
  rename: {
  suffix: '-360'
+ }
  },
+ {
  width: 540,
  rename: {
  suffix: '-540'
+ }
  },
+ {
  width: 720,
  rename: {
  suffix: '-720'
+ }
  },
+ {
  width: 900,
  rename: {
  suffix: '-900'
+ }
  },
+ {
  width: 1080,
  rename: {
  suffix: '-1080'
+ }
  },
+ {
  width: 1296,
  rename: {
  suffix: '-1296'
+ }
  },
+ {
  width: 1512,
  rename: {
  suffix: '-1512'
+ }
  },
+ {
  width: 1728,
  rename: {
  suffix: '-1728'
+ }
  },
+ {
  width: 2048,
  rename: {
  suffix: '-2048'
- },
+ }
  },
  {
  rename: {
@@ -67,6 +83,6 @@ gulp.task('default', function () {
  skipOnEnlargement: true,
  withoutEnlargement: true
  })
- ).pipe(gulp.dest('dist/images'))
+ ).pipe(gulp.dest('dist/images/news'))
 })
  
